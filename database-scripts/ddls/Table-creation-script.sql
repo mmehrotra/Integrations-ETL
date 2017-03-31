16043dfaf4dd884b7ac464a4a5367cd8fa2a7e03
mmehro <mmehrotra@sapient.com>
2017-03-31 17:17:20 +0530

    change

--- database-scripts/ddls/Table-creation-script.sql
+++ database-scripts/ddls/Table-creation-script.sql
@@ -28,30 +28,31 @@ CREATE TABLE [T_STG_CARGO_CARRYING_SHIPS] (
     [TEU] varchar(255)
 );
 
--- DROP TABLE T_CARGO_CARRYING_SHIPS;
+-- DROP TABLE T_CARGO_CARRYING_SHIP;
 -- TRUNCATE TABLE T_CARGO_CARRYING_SHIP;
 -- SELECT * FROM T_CARGO_CARRYING_SHIP;
 -- Script for creation of the main tables
 -- TODO :: create the primary key and other constraints
 CREATE TABLE [T_CARGO_CARRYING_SHIP] (
-    [LRIMOShipNo] varchar(255),
-    [Latitude] varchar(255),
-    [Longitude] varchar(255),
+	[ID] bigint IDENTITY(1,1) PRIMARY KEY,
+    [LRIMOShipNo] bigint,
+    [Latitude] float,
+    [Longitude] float,
     [ShipName] varchar(255),
     [ShipType] varchar(255),
-    [Draught] varchar(255),
-    [Beam] varchar(255),
-    [Length] varchar(255),
+    [Draught] float,
+    [Beam] float,
+    [Length] float,
     [CallSign] varchar(255),
-    [MMSI] varchar(255),
-    [Speed] varchar(255),
-    [Heading] varchar(255),
+    [MMSI] bigint,
+    [Speed] float,
+    [Heading] float,
     [Destination] varchar(255),
     [DestinationTidied] varchar(255),
-    [ETA] varchar(255),
+    [ETA] DATETIME,
     [AdditionalInfo] varchar(255),
-    [MovementDateTime] varchar(255),
-    [MovementID] varchar(255),
+    [MovementDateTime] DATETIME,
+    [MovementID] bigint,
     [MoveStatus] varchar(255),
     [TEU] varchar(255),
     [REC_CREATION_TIME] DATETIME,
@@ -62,6 +63,7 @@ CREATE TABLE [T_CARGO_CARRYING_SHIP] (
 );
 
 -- Staging table for Cargo's TblLastPortOfCall
+-- TRUNCATE TABLE [T_STG_CARGO_TBL_LAST_PORT_OF_CALL];
 CREATE TABLE [T_STG_CARGO_TBL_LAST_PORT_OF_CALL] (
     [MMSI] varchar(255),
     [LRNO] varchar(255),
@@ -72,4 +74,25 @@ CREATE TABLE [T_STG_CARGO_TBL_LAST_PORT_OF_CALL] (
     [ArrDate] varchar(255),
     [SailDate] varchar(255),
     [PortCallId] varchar(255)   
+);
+
+-- DROP TABLE [T_CARGO_TBL_LAST_PORT_OF_CALL]
+-- TRUNCATE TABLE T_CARGO_TBL_LAST_PORT_OF_CALL
+-- SELECT * FROM T_CARGO_TBL_LAST_PORT_OF_CALL
+-- Main table for Cargo's TblLastPortOfCall
+CREATE TABLE [T_CARGO_TBL_LAST_PORT_OF_CALL] (
+    [MMSI] varchar(255),
+    [LRNO] varchar(255),
+    [Port_ID] varchar(255),
+    [CountryCode] varchar(255),
+    [Country] varchar(255),
+    [LastPortOfCall] varchar(255),
+    [ArrDate] varchar(255),
+    [SailDate] varchar(255),
+    [PortCallId] varchar(255),
+    [REC_CREATION_TIME] DATETIME,
+    [REC_CREATED_BY] VARCHAR(255),
+    [REC_UPDATED_TIME] DATETIME,
+    [REC_UPDATED_BY] VARCHAR(255),
+    [SOURCE_FILE_NAME] VARCHAR(255)   
 );
